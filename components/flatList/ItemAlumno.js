54f305b21fd32aff2f8f7ea8d5099bc8be25f41a
royscript <roystandenb@gmail.com>
2022-05-12 10:57:11 -0400

    Agregado numero Matricula

--- components/flatList/ItemAlumno.js
+++ components/flatList/ItemAlumno.js
@@ -22,22 +22,20 @@ function ItemAlumno({ item,index, mostrarDetalles }) {
     <TouchableOpacity onPress={()=>{
               mostrarDetalles({ datos : item });
           }}>
-    <View style={styles(item.esPie).listItem}>
-      <Text style={{backgroundColor:'#4285f4', color:'#fff', textAlign:'center',borderRadius:30, width:20, height:20, top:50}}>{index+1}</Text>
-      <Image source={{uri:`https:\/\/drive.google.com\/uc?id=${item.foto}`}}  style={{width:60, height:60,borderRadius:30}} />
-      <View style={{alignItems:"center",flex:1}}>
-        <Text style={{fontWeight:"bold"}}>{item.nombres}</Text>
-        <Text>{item.apPaterno+' '+item.apMaterno}</Text>
-        <Text style={{fontWeight:"bold", color:"black"}}>{item.curso}</Text>
-        <Text style={{backgroundColor:'#562990', color:'#fff', textAlign:'center',borderRadius:30, height:20}}> N° Matricula {item.numero} </Text>
-        
-        {item.fechaRetiro==undefined? null : <Text style={{fontWeight:"bold", color:"black"}}> Fecha retiro : <Text style={{fontWeight:"bold", color:"red"}}>{item.fechaRetiro}</Text></Text>} 
-        {item.esPie=='SI'?<Text style={{fontWeight:"bold", color:"black", backgroundColor:'yellow'}}>PIE - {item.tipoPie}</Text>:null}
-        {item.esAlumnoEspecial==true?<Text style={{fontWeight:"bold", color:"white", backgroundColor:'green'}}>SEMI PRESENCIAL</Text>:null}
+      <View style={styles(item.esPie).listItem}>
+        <Text style={{backgroundColor:'#4285f4', color:'#fff', textAlign:'center',borderRadius:30, width:20, height:20, top:50}}>{index+1}</Text>
+        <Image source={{uri:`https:\/\/drive.google.com\/uc?id=${item.foto}`}}  style={{width:60, height:60,borderRadius:30}} />
+        <View style={{alignItems:"center",flex:1}}>
+          <Text style={{fontWeight:"bold"}}>{item.nombres}</Text>
+          <Text>{item.apPaterno+' '+item.apMaterno}</Text>
+          <Text style={{fontWeight:"bold", color:"black"}}>{item.curso}</Text>
+          <Text style={{backgroundColor:'#562990', color:'#fff', textAlign:'center',borderRadius:30, height:20}}> N° Matricula {item.numero} </Text>
+          
+          {item.fechaRetiro==undefined? null : <Text style={{fontWeight:"bold", color:"black"}}> Fecha retiro : <Text style={{fontWeight:"bold", color:"red"}}>{item.fechaRetiro}</Text></Text>} 
+          {item.esPie=='SI'?<Text style={{fontWeight:"bold", color:"black", backgroundColor:'yellow'}}>PIE - {item.tipoPie}</Text>:null}
+          {item.esAlumnoEspecial==true?<Text style={{fontWeight:"bold", color:"white", backgroundColor:'green'}}>SEMI PRESENCIAL</Text>:null}
+        </View>
       </View>
-      
-      
-    </View>
     </TouchableOpacity>
   );
 }
